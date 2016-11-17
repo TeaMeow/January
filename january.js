71e73b7f23904642e30b47e7d0a90984741da256
Yami Odymel <yamiodymel@gmail.com>
2016-11-17 22:44:59 +0800

    Update january.js

--- january.js
+++ january.js
@@ -1,4 +1,4 @@
-function generate(selectedYear, selectedMonth, options)
+export function generate(selectedYear, selectedMonth, options)
 {
     var years       = [],
         months      = [],
@@ -9,7 +9,8 @@ function generate(selectedYear, selectedMonth, options)
         cDay        = d.getDate(),
         leadingZero = options !== undefined && options.leadingZero !== undefined ? options.leadingZero : false,
         toFuture    = options !== undefined && options.toFuture    !== undefined ? options.toFuture    : false,
-        offset      = options !== undefined && options.offset      !== undefined ? options.offset      : 101
+        offset      = options !== undefined && options.offset      !== undefined ? options.offset      : 0,
+        range       = options !== undefined && options.range       !== undefined ? options.range       : 101
 
     /**
      * Year
@@ -17,14 +18,14 @@ function generate(selectedYear, selectedMonth, options)
 
     if(toFuture)
     {
-        var maxYear = cYear + offset
-        for(var y = cYear; y < maxYear; y++)
+        var maxYear = cYear + range + offset
+        for(var y = cYear + offset; y < maxYear; y++)
             years.push(y)
     }
     else
     {
-        var minYear = cYear - offset
-        for(var y = cYear; y > minYear; y--)
+        var minYear = cYear - range - offset
+        for(var y = cYear - offset; y > minYear; y--)
             years.push(y)
     }
 
@@ -42,14 +43,15 @@ function generate(selectedYear, selectedMonth, options)
      * Day
      */
 
-    var totalDay = 31
-
     if(selectedMonth !== undefined && selectedMonth !== false)
     {
         var isLeapYear = ((selectedYear % 4 == 0) && (selectedYear % 100 != 0)) || (selectedYear % 400 == 0),
             dayMap     = [31, (isLeapYear ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
             totalDay   = dayMap[selectedMonth - 1]
     }
+
+    if(totalDay === undefined)
+        var totalDay = 31
 
     for(var d = 1; d <= totalDay; d++)
         if(leadingZero && d < 10)
